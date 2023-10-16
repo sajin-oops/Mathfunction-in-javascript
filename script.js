d674e3f4a35ba1959f1082494782927a008492ef
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2023-10-16 18:51:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// - mathfunction in javascript
+
+console.log(Math.round(5.8))
+//output - 6
+console.log(Math.floor(5.8))
+//output - 5
+console.log(Math.ceil(5.1))
+// output - 6
+console.log(Math.round(5.1))
+// output - 5
+console.log(Math.PI)
+// output - 3.141592653589793
+console.log(Math.trunc(2,1))
+// output - 4. returns the integer part of x
+console.log(Math.pow(8,2))
+// output - 64
+console.log(Math.sqrt(36))
+// output - 6
+
+
+
